d918c3843c7f3bef6b8461306474253c9959b14b
Corsi <corsiferrao@gmail.com>
2025-09-17 16:34:29 -0300

    fix

--- main/main.c
+++ main/main.c
@@ -16,30 +16,11 @@
 // === Definições para SSD1306 ===
 ssd1306_t disp;
 
-volatile int flag_BTN_R = 0;
-volatile int flag_BTN_G = 0;
-volatile int flag_BTN_B = 0;
+QueueHandle_t xQueueBtn;
 
 // == funcoes de inicializacao ===
 void btn_callback(uint gpio, uint32_t events) {
-    printf("GPIO %d, events: %d\r\n", gpio, events);
-    if (events & 0x4) {
-        if (gpio == BTN_PIN_R)
-            flag_BTN_R = 1;
-        else if (gpio == BTN_PIN_G)
-            flag_BTN_G = 1;
-        else if (gpio == BTN_PIN_B)
-            flag_BTN_B = 1;
-    }
-
-    if (events == 0x8) {
-        if (gpio == BTN_PIN_R)
-            flag_BTN_R = 0;
-        else if (gpio == BTN_PIN_G)
-            flag_BTN_G = 0;
-        else if (gpio == BTN_PIN_B)
-            flag_BTN_B = 0;
-    }
+    if (events & GPIO_IRQ_EDGE_FALL) xQueueSendFromISR(xQueueBtn, &gpio, 0);
 }
 
 void oled_display_init(void) {
@@ -53,124 +34,90 @@ void oled_display_init(void) {
     ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
     ssd1306_clear(&disp);
     ssd1306_show(&disp);
-
-  //  gpio_put(LED_PIN_R, 0);
-   // gpio_put(LED_PIN_G, 0);
-   // gpio_put(LED_PIN_B, 0);
-  //  ssd1306_draw_string(&disp, 8, 24, 2, "Hello");
-  //  ssd1306_show(&disp);
-  //  sleep_ms(1000);
-  //  ssd1306_clear(&disp);
-   // ssd1306_show(&disp);
 }
 
 void btns_init(void) {
-    // Inicialização dos botões e LEDs
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-    
+
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
-    
     gpio_init(BTN_PIN_B);
     gpio_set_dir(BTN_PIN_B, GPIO_IN);
     gpio_pull_up(BTN_PIN_B);
 
-
-
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
-    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,  true);
-    gpio_set_irq_enabled(BTN_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,  true);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R,
+                                       GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+                                       true, &btn_callback);
+    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+                         true);
+    gpio_set_irq_enabled(BTN_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+                         true);
 }
 
 void led_rgb_init(void) {
-    /*
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 1);
+
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
     gpio_put(LED_PIN_G, 1);
-    gpio_init(LED_PIN_B);
-    gpio_set_dir(LED_PIN_B, GPIO_OUT);
-    gpio_put(LED_PIN_B, 1); */
-
-     gpio_init(LED_PIN_R);
-    gpio_set_dir(LED_PIN_R, GPIO_OUT);
-    gpio_put(LED_PIN_R, 0);
-
-    gpio_init(LED_PIN_G);
-    gpio_set_dir(LED_PIN_G, GPIO_OUT);
-    gpio_put(LED_PIN_G, 0);
 
     gpio_init(LED_PIN_B);
     gpio_set_dir(LED_PIN_B, GPIO_OUT);
-    gpio_put(LED_PIN_B, 0);
-
-    // sleep_ms(500);
-    // gpio_put(LED_PIN_R, 1);
-    // sleep_ms(500);
-    // gpio_put(LED_PIN_G, 1);
-    // sleep_ms(500);
-    // gpio_put(LED_PIN_B, 1);
-
-   
+    gpio_put(LED_PIN_B, 1);
 }
 
 void task_1(void *p) {
-    printf("Task 1 running\r\n");
-    
     btns_init();
     led_rgb_init();
     oled_display_init();
-    
+
+    uint btn_data;
+
     while (1) {
-       
-       
-        
+        if (xQueueReceive(xQueueBtn, &btn_data, pdMS_TO_TICKS(2000))) {
+            printf("btn: %d \n", btn_data);
 
-       if (flag_BTN_B == 1) {
-            gpio_put(LED_PIN_B, 0);
-         ssd1306_draw_string(&disp, 8, 24, 2, "BLUE");
-         ssd1306_show(&disp); 
-           
+            switch (btn_data) {
+                case BTN_PIN_B:
+                    gpio_put(LED_PIN_B, 0);
+                    ssd1306_draw_string(&disp, 8, 0, 2, "BLUE");
+                    ssd1306_show(&disp);
+                    break;
+                case BTN_PIN_G:
+                    gpio_put(LED_PIN_G, 0);
+                    ssd1306_draw_string(&disp, 8, 24, 2, "GREEN");
+                    ssd1306_show(&disp);
+                    break;
+                case BTN_PIN_R:
+                    gpio_put(LED_PIN_R, 0);
 
-        }
-
-
-        if (flag_BTN_G == 1) {
-            gpio_put(LED_PIN_G, 0);
-        ssd1306_draw_string(&disp, 8, 24, 2, "GREEN");
-        ssd1306_show(&disp);
-        }
-
-
-
-         if (flag_BTN_R == 1) {
-            gpio_put(LED_PIN_R, 0);
-        ssd1306_draw_string(&disp, 8, 24, 2, "RED");
-       ssd1306_show(&disp);
-        }
-
-
-        if( flag_BTN_R == 0 && flag_BTN_G == 0 && flag_BTN_B == 0) {
-         ssd1306_clear(&disp);
-         ssd1306_show(&disp);
+                    ssd1306_draw_string(&disp, 8, 48, 2, "RED");
+                    ssd1306_show(&disp);
+                    break;
+                default:
+                    // Handle other buttons if needed
+                    break;
+            }
+        } else {
+            ssd1306_clear(&disp);
+            ssd1306_show(&disp);
             gpio_put(LED_PIN_R, 1);
-             gpio_put(LED_PIN_G, 1);
-              gpio_put(LED_PIN_B, 1);
+            gpio_put(LED_PIN_G, 1);
+            gpio_put(LED_PIN_B, 1);
         }
-
-        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 int main() {
     stdio_init_all();
 
+    xQueueBtn = xQueueCreate(32, sizeof(uint));
     xTaskCreate(task_1, "Task 1", 8192, NULL, 1, NULL);
 
     vTaskStartScheduler();
